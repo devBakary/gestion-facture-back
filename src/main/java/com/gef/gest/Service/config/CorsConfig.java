a0ed5f960568e9384730f5a8042008544473c3d7
devBakary <bakdia194@gmail.com>
2026-05-20 13:07:17 +0000

    fix error android

--- src/main/java/com/gef/gest/Service/config/CorsConfig.java
+++ src/main/java/com/gef/gest/Service/config/CorsConfig.java
@@ -19,7 +19,10 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:4200","https://gest-facture.netlify.app" )
+                List.of("http://localhost:4200",
+                        "https://gest-facture.netlify.app",
+                        "capacitor://localhost",
+                        "http://localhost" )
         );
 
         configuration.setAllowedMethods(
--- src/main/java/com/gef/gest/Service/config/SecurityConfig.java
+++ src/main/java/com/gef/gest/Service/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
 
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
